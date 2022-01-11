05ae73b4be50a9a216fc677fedd9a7d2bcd86985
Tony Ware <ton8675309@gmail.com>
2022-01-11 16:03:19 -0700

    made changes to service and model

--- server/services/CarsService.js
+++ server/services/CarsService.js
@@ -20,6 +20,29 @@ class CarsService {
     return createdCar
   }
 
+  async remove(carId, creatorId) {
+    const foundCar = await this.getById(carId)
+    if (foundCar.creatorId.toString() !== creatorId) {
+      throw new BadRequest('Unauthorized to delete')
+    }
+    await foundCar.remove()
+    return foundCar
+  }
+  async edit(carId, editedCar) {
+    const carToEdit = await this.getById(carId)
+    if (carToEdit.creatorId.toString() !== editedCar.creatorId) {
+      throw new BadRequest('Unauthorized to edit')
+    }
+    carToEdit.year = editedCar.year || carToEdit.year
+    carToEdit.make = editedCar.make || carToEdit.make
+    carToEdit.model = editedCar.model || carToEdit.model
+    carToEdit.price = editedCar.price || carToEdit.price
+    carToEdit.color = editedCar.color || carToEdit.color
+    carToEdit.imgUrl = editedCar.imgUrl || carToEdit.imgUrl
+    await carToEdit.save()
+    return carToEdit
+  }
+
 }
 
 export const carsService = new CarsService();
